55270503541ff71df7b0a28e44251b40b109a825
kiddynamo <njenan@gmail.com>
2017-05-10 19:53:31 -0700

    Full fibonacci example tdd

--- src/test/java/Fibonacci.java
+++ src/test/java/Fibonacci.java
@@ -1,6 +1,10 @@
 
 public class Fibonacci {
     public static int get(int i) {
-        return 1;
+        if (i < 2) {
+            return 1;
+        } else {
+            return Fibonacci.get(i - 1) + Fibonacci.get(i - 2);
+        }
     }
 }
